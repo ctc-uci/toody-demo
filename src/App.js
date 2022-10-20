4e9584bc0f499f99321a4d9e5c4b4dc523b8d698
Jane Vo <vo.janehanh@gmail.com>
2022-10-20 01:40:12 -0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="page-container">
+        <div className="content-wrap">
+          <Routes>
+            <Route path="/" />
+            <Route path="/profile" />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
