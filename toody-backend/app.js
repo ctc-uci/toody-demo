3903add028d5f1582ae91145ba990fd1701089d5
Mingjia Wang <mingjiawangdev@gmail.com>
2022-10-29 17:34:48 -0700

    Added some console.logs

--- toody-backend/app.js
+++ toody-backend/app.js
@@ -5,13 +5,18 @@ require('dotenv').config();
 const Todo = require('./models/todo.model');
 const Note = require('./models/note.model');
 
+mongoose.set('debug', true);
+
 mongoose.connect(process.env.MONGO_URI, () => {
   console.log('connected to mongo');
 })
 
+
+
 const app = express();
 
 app.get('/todos/', async (req, res) => {
+  console.log('GET /todos hit');
   try {
     const todos = await Todo.find({});
     res.json(todos);
@@ -21,6 +26,7 @@ app.get('/todos/', async (req, res) => {
 });
 
 app.get('/notes/', async (req, res) => {
+  console.log('GET /notes hit');
   try {
     const notes = await Note.find({});
     res.json(notes);
