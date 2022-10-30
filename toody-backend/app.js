7e2014125f004bba94b23eb8b67d6a979d0da12d
Chris Tian <ctian1@users.noreply.github.com>
2022-10-30 01:45:10 -0700

    Move routes to separate files

--- toody-backend/app.js
+++ toody-backend/app.js
@@ -5,30 +5,22 @@ require('dotenv').config();
 const Todo = require('./models/todo.model');
 const Note = require('./models/note.model');
 
+const todosRouter = require('./routes/todos.router');
+const notesRouter = require('./routes/notes.router');
+
 mongoose.connect(process.env.MONGO_URI, () => {
   console.log('connected to mongo');
-})
+});
 
 const app = express();
 
-app.get('/todos/', async (req, res) => {
-  try {
-    const todos = await Todo.find({});
-    res.json(todos);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.use(
+  express.urlencoded({ extended: true }),
+  express.json(),
+);
 
-app.get('/notes/', async (req, res) => {
-  console.log('GET /notes hit');
-  try {
-    const notes = await Note.find({});
-    res.json(notes);
-  } catch (err) {
-    console.log(err);
-  }
-});
+app.use('/todos', todosRouter);
+app.use('/notes', notesRouter);
 
 app.listen(3001, () => {
   console.log('server listening on port 3001');
